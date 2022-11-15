08ebf1ee7ec1311a1ed2ec17a474a25d5f2faadc
Melanie Morey <morey.melanie@gmail.com>
2022-11-15 17:49:04 +0100

    header +

---
+++ main.cs
@@ -0,0 +1,43 @@
+/* _functions */
+/* _mixins */
+/* _extensions */
+*,
+*::after,
+*::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: inherit;
+}
+
+body {
+  background-color: #FFF;
+}
+
+body {
+  font-family: "Roboto", sans-serif;
+  color: #000;
+}
+
+h1 {
+  font-family: "Shrikhand", cursive;
+}
+
+/* components/buttons */
+.header {
+  display: flex;
+  background: #9356DC;
+}
+
+@media only screen and (max-width : 480px) {
+  .header {
+    background: #FF79DA;
+  }
+}
+/*   layout/nav
+    layout/container
+    layout/form     */
+/*  pages/work
+    pages/about
+    pages/project   */
+
+/*# sourceMappingURL=main.cs.map */
